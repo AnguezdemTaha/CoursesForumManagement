98429ac5233bf09c33b3c6601a9b16f54ca86094
tahaang <anguezdemtaha16@gmail.com>
2021-01-08 14:57:33 +0100

    actuel prj with sonar

--- src/java/com/servlet/Adm_accuiel.java
+++ src/java/com/servlet/Adm_accuiel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.servlet;
+package java.com.servlet;
 
 import dao.Module_filiere_anneeDao;
 import java.io.IOException;
